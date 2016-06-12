22161ee1205f41792d0afd8184ff48413b6f73ce
smnkrt <szymon.krywult@netguru.pl>
2016-06-12 21:33:46 +0200

    add support for button_to helper

--- lib/sweet-alert-confirm/view_helpers.rb
+++ lib/sweet-alert-confirm/view_helpers.rb
@@ -4,34 +4,46 @@ module SweetAlertConfirm
       html_options = args[block_given? ? 1 : 2] || {}
 
       if options_has_confirm?(html_options)
-        html_options['data-sweet-alert-confirm'] = html_options.delete(:confirm) ||
-                    html_options[:data].delete(:confirm)
+        html_options['data-sweet-alert-confirm'] = confirm_data(html_options)
       end
       super *args, &block
     end
 
     def submit_tag(value = 'Save changes', options = {})
-      options['data-sweet-alert-confirm'] = options.delete(:confirm) || options[:data].delete(:confirm) if options_has_confirm?(options)
+      if options_has_confirm?(options)
+        options['data-sweet-alert-confirm'] =  confirm_data(options)
+      end
       super value, options
     end
 
-  def button_tag(*args, &block)
-    html_options = args[block_given? ? 0 : 1] || {}
+    def button_tag(*args, &block)
+      html_options = args[block_given? ? 0 : 1] || {}
 
-    if options_has_confirm?(html_options)
-      html_options['data-sweet-alert-confirm'] = html_options.delete(:confirm) ||
-                  html_options[:data].delete(:confirm)
+      if options_has_confirm?(html_options)
+        html_options['data-sweet-alert-confirm'] = confirm_data(html_options)
+      end
+      super *args, &block
     end
-    super *args, &block
-  end
+
+    def button_to(*args, &block)
+      html_options = args[block_given? ? 1 : 2] || {}
+
+      if options_has_confirm?(html_options)
+        html_options['data-sweet-alert-confirm'] = confirm_data(html_options)
+      end
+      super *args, &block
+    end
 
     protected
+
+    def confirm_data(options)
+      options.delete(:confirm) || options[:data].delete(:confirm)
+    end
+
     def options_has_confirm?(options)
-      if (options[:confirm] || (options[:data] && options[:data][:confirm]))
-        true
-      else
-        false
-      end
+      confirm_present = options[:confirm] || (options[:data] && options[:data][:confirm])
+      return true if confirm_present.present?
+      false
     end
   end
 end
